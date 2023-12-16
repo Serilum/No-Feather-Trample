2bc10e4cdd2cd26d11e50683682618446220a8e7
Rick South <rick@natamus.com>
2023-12-16 13:21:34 +0100

    No Feather Trample source code for 1.19.2.

---
+++ Forge/src/main/java/com/natamus/nofeathertrample/forge/mixin/FarmBlockMixin.java
@@ -0,0 +1,34 @@
+package com.natamus.nofeathertrample.forge.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FarmBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = FarmBlock.class, priority = 1001)
+public abstract class FarmBlockMixin extends Block {
+	public FarmBlockMixin(Properties properties) {
+		super(properties);
+	}
+
+	@Inject(method = "fallOn(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/Entity;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/FarmBlock;turnToDirt(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V"), cancellable = true)
+	public void fallOn(Level level, BlockState blockState, BlockPos blockPos, Entity entity, float f, CallbackInfo ci) {
+		if (entity instanceof LivingEntity) {
+			LivingEntity livingEntity = (LivingEntity)entity;
+			if (EnchantmentHelper.getEnchantmentLevel(Enchantments.FALL_PROTECTION, livingEntity) > 0 || livingEntity.hasEffect(MobEffects.SLOW_FALLING)) {
+				ci.cancel();
+				super.fallOn(level, blockState, blockPos, entity, f);
+			}
+		}
+	}
+}
