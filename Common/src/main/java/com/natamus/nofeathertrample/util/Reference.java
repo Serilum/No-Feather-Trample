116f74569d18af35ea9a7d96b1f42fe2b12e5250
Rick South <rick@natamus.com>
2024-07-28 07:23:39 +0200

    (v1.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/nofeathertrample/util/Reference.java
+++ Common/src/main/java/com/natamus/nofeathertrample/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.nofeathertrample.util;
 public class Reference {
 	public static final String MOD_ID = "nofeathertrample";
 	public static final String NAME = "No Feather Trample";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
